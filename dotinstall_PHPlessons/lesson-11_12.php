cf97004a599f0c8c2d524850914a2d8586229836
Momoko Karasawa <gretel.leez@gmail.com>
2017-04-01 15:10:00 +0900

    lesson11-12 up

--- dotinstall_PHPlessons/lesson-11_12.php
+++ dotinstall_PHPlessons/lesson-11_12.php
@@ -1,9 +1,33 @@
 <?php  
 
 // #11 while文でループ処理をしてみよう
+// while
+// do ... while 
+
+$i = 0;
+while ($i < 10) {
+	echo $i;
+	$i++;
+}
+
+$j = 100;
+do {
+	echo $j;
+	$j++;
+}while ($j < 10);
 
 
 // #12 for文でループ処理をしてみよう
+// for
+// break: ループを抜ける
+// continue:　それ以降の処理を実行せずに次のループに移る
 
+for ($k = 0; $k < 10; $k++) { 
+	if ($k === 5) {
+		//break;
+		continue;
+	}
+	echo $k;
+}
 
 ?>
